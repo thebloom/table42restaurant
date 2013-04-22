4ab374a73984474a799587afb1e02cae2101951c
Max Sjöblom <max.sjoblom@aalto.fi>
2013-04-22 10:54:14 +0300

    capacity handler service added

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,15 +1,27 @@
-function MapCtrl($scope, $http) {
+function MapCtrl($scope, $http, cap) {
+  $scope.cap = cap;
   $http.get('tables.json').
   success(function(data) {
     console.log(data);
     $scope.tables = data.tables;
   });
+  console.log($scope.capacity);
   $scope.toggle = function(table) {
     table.open = !table.open;
+    cap.changeCapacity(table);
   };
+  
+  // $scope.capacity = function() {
+  //   $scope.cap = 0;
+  //   $.each($scope.tables, function(i, item) {
+  //     if(item.open) {
+  //       $scope.cap += item.capacity;
+  //     }
+  //   });
+  // }
 }
 
-function ReservationCtrl($scope, $http) {
+function ReservationCtrl($scope, $http, cap) {
   $scope.predicate = 'time';
   $scope.up = "icon-chevron-up";
   $scope.down = "icon-chevron-down";
@@ -24,4 +36,5 @@ function ReservationCtrl($scope, $http) {
   }
   $scope.active = 'open';
   $scope.incoming = [];
+  $scope.cap = cap;
 }
--- assets/js/modules.js
+++ assets/js/modules.js
@@ -1,10 +1,51 @@
-angular.module('table42', [])
-.config(['$routeProvider', '$compileProvider', function($routeProvider, $compileProvider) {
+var app = angular.module('table42', []);
+app.config(['$routeProvider', '$compileProvider', function($routeProvider, $compileProvider) {
 	$compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|app|file):/);
     $routeProvider.
       when('/map', {templateUrl: 'views/map.html',   controller: MapCtrl}).
       when('/reservation', {templateUrl: 'views/reservation.html',   controller: ReservationCtrl}).
       otherwise({redirectTo: '/map'});
 }]);
+
+app.factory('cap', function($http) {
+	var capacityService = {};
+	var cap = 0;
+	$http.get('tables.json').
+	success(function(data) {
+	    $.each(data.tables, function(i, item) {
+	      if(item.open) {
+	        cap += item.capacity;
+	      }
+	    });
+	    console.log(cap);
+	});
+	capacityService.changeCapacity = function(item) {
+		if(item.open) {
+			cap += item.capacity;
+		}
+		else {
+			cap -= item.capacity;
+		}
+		console.log(cap);
+	}
+	capacityService.capacity = function() {
+		return cap;
+	}
+    // var items = [];
+    // var myBasketService = {};
+
+    // myBasketService.addItem = function(item) {
+    //     items.push(item);
+    // };
+    // myBasketService.removeItem = function(item) {
+    //     var index = items.indexOf(item);
+    //     items.splice(index, 1);
+    // };
+    // myBasketService.items = function() {
+    //     return items;
+    // };
+
+    return capacityService;
+});
 
 
